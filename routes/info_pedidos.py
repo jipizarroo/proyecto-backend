535f391abff28572e36907dff414b173ece389a9
jassan leyva <jassan851031@gmail.com>
2020-02-11 22:25:35 -0300

    tablas completas

--- routes/info_pedidos.py
+++ routes/info_pedidos.py
@@ -1,25 +1,20 @@
 from flask import Blueprint, request, jsonify 
 from flask_jwt_extended import (
-    jwt_required
+    jwt_required, get_jwt_identity
 )
 from models import db, Info_Pedidos, Pedido
 
 route_info_pedidos = Blueprint('route_info_pedidos', __name__)
 
-
-#@jwt_required
-
+@jwt_required
 @route_info_pedidos.route('/info-pedidos', methods=['GET'])
 def info_pedido():
-    #Obtencion de id dede el token!!!!!
-    id_user = 2
-    #---------------------------------
-
     infos = Info_Pedidos.query.filter_by(id_user = id_user).all()
     infos = [info.serialize() for info in infos]
 
     return jsonify(infos), 200
 
+@jwt_required
 @route_info_pedidos.route('/info-pedidos/<int:id>', methods=['GET'])
 def info_pedido_id(id):
     info = Info_Pedidos.query.get(id)
